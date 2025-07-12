492c286d63c67527c9fe468f5e10cc5d61b7583f
Eric Macharia <iriqmash21@gmail.com>
2025-07-12 11:44:33 +0300

    corrections done to code

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,17 @@
-P = 1000
-R = 0.05
-T = 3
-interest = (P * R * T)
+# calculate simple interest earned.
+# define principal whcih is initial investment
+# P = $1000
+principal = 1000
+
+# define annual interest as decimal
+# R = 5% = 0.05
+rate = 0.05
+
+# define number of years
+# T = 3 years
+time = 3
+
+# calculate simple interest using formula: (I = P * R * T)
+# I = interest earned
+interest = principal * rate * time
 print(f"The simple interest is: {interest}")
