c7acc2dc2aade79a42f1c48db0915193bb12602c
Eric Macharia <iriqmash21@gmail.com>
2025-07-24 22:22:20 +0300

    Task 1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,35 +1,37 @@
 # simple interface for managing a shopping list
 
 def display_menu():
+    # display menu options to users
     print("Shopping List Manager")
-    print("-----" * 4)
-    print("Press 1 to add items")
-    print("Press 2 to remove an item")
-    print("Press 3 to view list")
-    print("Press 4 to exit")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
 def main():
     # implementation of an array shopping_list
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your selection: ")
+        choice = input("Enter your choice: ")
         if choice == "1":
             item_to_add= input("Please add the item you want: ")
             shopping_list.append(item_to_add)
-            print(f"{item_to_add} added to the shopping list successfully!")
+            pass
         elif choice == "2":
             item_to_remove = input("Please type item you wish to remove: ")
             if item_to_remove in shopping_list:
                 shopping_list.remove(item_to_remove)
-                print(f"{item_to_remove} removed from the shopping list successfully")
+                pass
             else:
-                print(f"{item_to_remove} not in the list")       
+                print(f"{item_to_remove} not in the list") 
+                pass      
         elif choice == "3":
             print(shopping_list)
+            pass
         elif choice == "4":
             print("Goodbye!")
-            exit()
+            break
         else:
             print("Invalid selection")
 main()
