22387785f8ae3780a855fa86574f96bd3a7199b8
Eric Macharia <iriqmash21@gmail.com>
2025-07-24 22:32:07 +0300

    Task 1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,12 +13,12 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
-        if choice == 1:
+        choice = input("Enter your choice: ").strip()
+        if choice == '1':
             item_to_add= input("Please add the item you want: ")
             shopping_list.append(item_to_add)
             pass
-        elif choice == 2:
+        elif choice == '2':
             item_to_remove = input("Please type item you wish to remove: ")
             if item_to_remove in shopping_list:
                 shopping_list.remove(item_to_remove)
@@ -26,10 +26,10 @@ def main():
             else:
                 print(f"{item_to_remove} not in the list") 
                 pass      
-        elif choice == 3:
+        elif choice == '3':
             print(shopping_list)
             pass
-        elif choice == 4:
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
