7a1e298c71b4f1e8dff7b760327cbc13f05c8e35
Eric Macharia <iriqmash21@gmail.com>
2025-07-24 20:26:54 +0300

    Task 0 arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,19 @@
 # function for basic arithmetic
 
-def perform_operation(num1, num2, operation):
+def perform_operation(num1: float, num2: float, operation: str):
     operation = "add", "subtract", "multiply", "divide"
-    if num2 == 0:
-        return f"Cannot divide by zero"
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return f"Cannot divide by zero"
+            else:
+                return num1 / num2
+        case _:
+            return f"Invalid operation"
     return operation
