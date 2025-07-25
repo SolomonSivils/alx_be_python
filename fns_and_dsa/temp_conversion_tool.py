cd37ddbe79dbd34eb0938d2adce6ab408fc41387
Eric Macharia <iriqmash21@gmail.com>
2025-07-25 20:04:42 +0300

    Task 3

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,8 +4,8 @@
 # C = (F - 32) * 5/9
 # F = C * 9/5 + 32
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_fahrenheit(celsius):
     # F = C * 9/5 + 32
@@ -18,19 +18,23 @@ def convert_to_celsius(fahrenheit):
     return celsius
 
 def main():
-    temp_input = float(input("Enter the temperature to convert: "))
-    unit_input = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+    try:
 
-    if unit_input == "F":
-        converted_temp = convert_to_celsius(temp_input)
-        print(f"{temp_input}F is {converted_temp}C")
-    elif unit_input == "C":
-        converted_temp = convert_to_fahrenheit(temp_input)
-        print(f"{temp_input}C is {converted_temp}F")
-    else: 
-        #handle invalid input
+        temp_input_val = input("Enter the temperature to convert: ")
+        temp_input = float(temp_input_val)
+        unit_input = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+
+        if unit_input == "F":
+            converted_temp = convert_to_celsius(temp_input)
+            print(f"{temp_input}°F is {converted_temp}°C")
+        elif unit_input == "C":
+            converted_temp = convert_to_fahrenheit(temp_input)
+            print(f"{temp_input}°C is {converted_temp}°F")
+        else: 
+            #handle invalid input
+            print("Invalid unit. Please select C for celsius and F for Fahrenheit")
+    except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
-
 
 if __name__ == "__main__":
     main()
